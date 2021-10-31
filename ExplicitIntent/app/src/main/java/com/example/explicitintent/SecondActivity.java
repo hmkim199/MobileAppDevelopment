60468ce0670949e809c57bc6cf66c3b4055f3028
hmkim199 <hmkim199@gmail.com>
2021-10-31 11:48:12 +0900

    feat: add intent

--- ExplicitIntent/app/src/main/java/com/example/explicitintent/SecondActivity.java
+++ ExplicitIntent/app/src/main/java/com/example/explicitintent/SecondActivity.java
@@ -3,14 +3,28 @@ package com.example.explicitintent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.explicitintent.databinding.ActivityMainBinding;
+import com.example.explicitintent.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
 
-    
+    private ActivitySecondBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        binding = ActivitySecondBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras == null){
+            return ;
+        }
+
+        String qString = extras.getString("qString");
+        binding.textView2.setText(qString);
     }
 }
