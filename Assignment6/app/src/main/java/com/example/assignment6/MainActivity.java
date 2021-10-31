e072a65e84e0c2d331f2621b40c4e2778d62426b
hmkim199 <hmkim199@gmail.com>
2021-11-01 03:04:51 +0900

    add assignment 6

---
+++ Assignment6/app/src/main/java/com/example/assignment6/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.assignment6;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.assignment6.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private ActivityResultLauncher<Intent> resultLauncher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        resultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode() == RESULT_OK){
+                            Intent data = result.getData();
+
+                            String returnString = data.getExtras().getString("returnData");
+                            binding.textView1.setText(returnString);
+                        }
+                    }
+                }
+        );
+    }
+
+    public void sendText(View view){
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        String myString = binding.editText1.getText().toString();
+        intent.putExtra("qString", myString);
+        resultLauncher.launch(intent);
+    }
+}
